5739bbfaa828c9454a1f62b284eb80eb65fec520
tony.chen <mfc6000@126.com>
2025-09-18 14:27:15 +0800

    Update repoFilesRouter.js

--- src/routers/repoFilesRouter.js
+++ src/routers/repoFilesRouter.js
@@ -11,4 +11,13 @@ router.get('/:repoId/files', (req, res) => {
   });
 });
 
+router.post('/:repoId/files', (req, res) => {
+  res.status(501).json({
+    error: {
+      code: 'NOT_IMPLEMENTED',
+      message: 'List repository files is not implemented yet.',
+    },
+  });
+});
+
 module.exports = router;
