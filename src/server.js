cb5b128656b61e5981fe9f16a99941ba8162c2c7
Tony Chen <tony.chen.cjun@gmail.com>
2025-09-20 14:28:08 +0800

    add 0.0.0.0 to ask listen routing ipv4

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ async function start() {
 
   const app = createApp();
 
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server listening on http://0.0.0.0:${PORT}`);
   });
 }
 
